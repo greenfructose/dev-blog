6fd8ff3c0607419a21e9915c3677cdc7b45c1eab
greenfructose <justinclarkturney@gmail.com>
2022-05-04 09:56:11 -0500

    Created PostCard

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { PostCard, Categories, PostWidget } from '../components/';
 import { getPosts } from '../services';
 
 
-export default function Home() {
+export default function Home( {posts} ) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -14,7 +14,7 @@ export default function Home() {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
--- services/index.js
+++ services/index.js
@@ -3,36 +3,37 @@ import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-    const query = gql `
-        query MyQuery {
-            postsConnection {
-            edges {
-                node {
-                author {
-                    bio
-                    id
-                    name
-                    photo {
-                    url
-                    }
-                }
-                createdAt
-                slug
-                title
-                excerpt
-                featuredImage {
-                    url
-                }
-                categories {
-                    name
-                    slug
-                }
-                }
+    const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
             }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
             }
+            categories {
+              name
+              slug
+            }
+          }
         }
-    `
+      }
+    }
+  `;
     const result = await request(graphqlAPI, query)
 
-    return result.postConnection.edges;
+    return result.postsConnection.edges;
 }
