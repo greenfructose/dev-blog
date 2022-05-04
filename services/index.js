196856558e654038b0e1f1b13c53ebde3aca03d1
greenfructose <justinclarkturney@gmail.com>
2022-05-04 13:57:19 -0500

    Created component boilerplates for post detail

--- services/index.js
+++ services/index.js
@@ -7,36 +7,70 @@ export const getPosts = async () => {
     query MyQuery {
       postsConnection {
         edges {
-          cursor
-          node {
+            cursor
+            node {
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                    }
+                }
+            }
+        }
+    }
+  `;
+    const result = await request(graphqlAPI, query)
+
+    return result.postsConnection.edges;
+};
+
+export const getPostDetails = async (slug) => {
+    const query = gql`
+    query GetPostDetails($slug: String!) {
+        post(where: { slug: $slug }){
             author {
-              bio
-              name
-              id
-              photo {
-                url
-              }
+                bio
+                name
+                id
+                photo {
+                    url
+                }
             }
             createdAt
             slug
             title
             excerpt
             featuredImage {
-              url
+                url
             }
             categories {
-              name
-              slug
+                name
+                slug
+            }
+            content {
+                raw
             }
           }
         }
-      }
-    }
   `;
-    const result = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query, { slug })
 
-    return result.postsConnection.edges;
-}
+    return result.post;
+};
 
 export const getRecentPosts = async () => {
     const query = gql`
@@ -59,7 +93,7 @@ export const getRecentPosts = async () => {
     return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
@@ -75,7 +109,7 @@ export const getSimilarPosts = async () => {
             }
         }
     `
-    const result = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query, { categories, slug })
 
     return result.posts;
 }
