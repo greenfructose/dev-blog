435791e7a85ec2a46411483d4de2c77fb8045f27
greenfructose <justinclarkturney@gmail.com>
2022-05-05 16:45:02 -0500

    New og image

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
           property="og:description"
           content="Dev blog written with Next.js and GraphQL, styled with TailwindCSS and hosted on Vercel"
         />
-        <meta property="og:image" content="https://dev-blog-delta.vercel.app/favicon.ico" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
